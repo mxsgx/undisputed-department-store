ee18961d3dabf1c3b8ea0f6d769284dd975e7070
mxsgx <45129872+mxsgx@users.noreply.github.com>
2024-07-26 08:45:59 +0700

    fix: table `orders` is missing

--- database/migrations/2024_07_25_165640_create_orders_table.php
+++ database/migrations/2024_07_25_165640_create_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->text('customer_note')->nullable();
             $table->ipAddress('customer_ip_address')->nullable();
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('order');
+        Schema::dropIfExists('orders');
     }
 };
